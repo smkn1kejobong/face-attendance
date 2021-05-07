5dfc076cea214b9e29ec4fa5de03be305d749719
shinau21 <bandingandesa@gmail.com>
2021-05-07 16:22:55 +0700

    Fix 'numpy.ndarray' object is not callable

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -1,5 +1,3 @@
-#!/bin/env python3
-
 import cv2
 import numpy as np
 import face_recognition
@@ -20,13 +18,13 @@ def findEncodings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)
+        encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
 encodeListKnown = findEncodings(images)
 print(len(encodeListKnown))
-#print("Encoding Complete")
+print("Encoding Complete")
 #
 cap = cv2.VideoCapture(0)
 #
@@ -42,15 +40,15 @@ while True:
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
         print(faceDis)
-        matchIndex = np.linalg.norm(faceDis)
+        matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name)
             y1,x2,y2,x1 = faceLoc
             y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img(x1,y2-35),(x2,y2),(0.255,0),cv2.FILLED)
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
     
     cv2.imshow('WebCam',img)
